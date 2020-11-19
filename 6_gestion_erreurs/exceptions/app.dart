947eb46a3b9682ed3114eb8d50b485538fc79d49
Maxime Britto <maxime@purplegiraffe.fr>
2020-11-19 11:12:26 +0100

    16_3_Traiter_les_erreurs_de_façon_specifiques_en_Dart

--- 6_gestion_erreurs/exceptions/app.dart
+++ 6_gestion_erreurs/exceptions/app.dart
@@ -1,6 +1,6 @@
 void main() {
   final listeDesScores = [125, 245, 87, 96, 324];
-  afficheLesPremiers(listeDesScores, nombreDeVainqueurs: 3);
+  afficheLesPremiers(listeDesScores, nombreDeVainqueurs: 30);
   print("Félicitation aux vainqueurs");
 }
 
@@ -12,6 +12,9 @@ void afficheLesPremiers(List<int> scores, {int nombreDeVainqueurs = 1}) {
       print(scoresTries[i]);
     }
     print("Affichage terminé");
+  } on RangeError catch (e) {
+    print(
+        "Il n'y a pas assez de participants (${e.end + 1}) pour afficher $nombreDeVainqueurs gagnants");
   } catch (e) {
     print(e);
   }
