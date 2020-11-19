7d924f1aa0f7edb8b50a2f10765d6cf09e0d77fd
Maxime Britto <maxime@purplegiraffe.fr>
2020-11-19 11:05:09 +0100

    16_2_Gerer_les_Exceptions_en_Dart

--- 6_gestion_erreurs/exceptions/app.dart
+++ 6_gestion_erreurs/exceptions/app.dart
@@ -1,13 +1,18 @@
 void main() {
   final listeDesScores = [125, 245, 87, 96, 324];
-  afficheLesPremiers(listeDesScores, nombreDeVainqueurs: 10);
+  afficheLesPremiers(listeDesScores, nombreDeVainqueurs: 3);
   print("Félicitation aux vainqueurs");
 }
 
 void afficheLesPremiers(List<int> scores, {int nombreDeVainqueurs = 1}) {
   final scoresTries = List<int>.from(scores);
   scoresTries.sort((a, b) => b.compareTo(a));
-  for (var i = 0; i < nombreDeVainqueurs; i++) {
-    print(scoresTries[i]);
+  try {
+    for (var i = 0; i < nombreDeVainqueurs; i++) {
+      print(scoresTries[i]);
+    }
+    print("Affichage terminé");
+  } catch (e) {
+    print(e);
   }
 }
